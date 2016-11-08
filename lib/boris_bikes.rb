fc4f1b4792eed9f64d6ac9a391f41c8f70be09d7
George Seeger <georgeseeger@hotmail.com>
2016-11-08 16:57:55 +0000

    steps 13 - 14

--- lib/boris_bikes.rb
+++ lib/boris_bikes.rb
@@ -1,20 +1,24 @@
 class DockingStation
 
-  attr_reader :bike_count
+  attr_reader :bike_count, :bikes
 
   def initialize
-    @bike_count = 10
+    @bikes = Array.new(20){ Bike.new }
+    @bike_count = @bikes.size
   end
 
-  def release_bike
-    fail "Fail" if @bike_count < 1
-    @bike_count -= 1
-    Bike.new
+  def release_bike number = 1
+    fail "Fail" if @bike_count < number
+    @bike_count -= number
+    (number -1 ).times{ @bikes.pop }
+    @bikes.pop
   end
 
   def dock_bike bike
-    fail "Fail" if @bike_count >= 10
+    fail "Fail" if @bike_count >= 20
     @bike_count += 1
+    if bike.instance_of?(Bike) then @bikes << bike
+    else raise "Not a bike argument" end
     return nil
   end
 
--- spec/boris_bikes_spec.rb
+++ spec/boris_bikes_spec.rb
@@ -34,19 +34,35 @@ describe DockingStation do
    end
 
    it "should have nine bikes in the dock, and then ten after docking a bike" do
-     expect(dock.bike_count).to eq 9
+     expect(dock.bike_count).to eq 19
      dock.dock_bike(bike)
-     expect(dock.bike_count).to eq 10
+     expect(dock.bike_count).to eq 20
    end
 
    it "should raise an error upon releasing bike when no bikes are left in dock" do
-     10.times { dock.release_bike }
+     dock.bike_count.times { dock.release_bike }
      expect{dock.release_bike}.to raise_error("Fail")
    end
 
-   it "should raise an error upon docking bike when 10 bikes in dock" do
-     10.times {dock.dock_bike(bike)}
+   it "should raise an error upon docking bike when 20 bikes in dock" do
+     20.times {dock.dock_bike(bike)}
      expect {dock.dock_bike(bike)}.to raise_error("Fail")
+   end
+
+   it "should be able to release multiple bikes" do
+     number = rand(5)
+     expect(dock).to respond_to(:release_bike).with(1).argument
+     before = dock.bike_count
+     dock.release_bike(number)
+     expect(dock.bike_count).to eq (before - number)
+   end
+
+   it "should have a collection of bikes" do
+
+     dock.bikes.each{ |b|
+       expect(b.instance_of?(Bike)).to eq true
+     }
+
    end
 
 end
