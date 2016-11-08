9d98ccf19c9c60fcc9e4d69fb00c65b7dd6a4704
James <jamesdix@cwgsy.net>
2016-11-08 13:25:23 +0000

    Step 8 Complete

--- spec/boris_bikes_spec.rb
+++ spec/boris_bikes_spec.rb
@@ -1,10 +1,13 @@
 require "boris_bikes"
 
-describe "Docking Station" do
+describe DockingStation do
   dock = DockingStation.new
   it "should return a new DockingStation object" do
 
     expect(dock.instance_of?(DockingStation)).to be_truthy
   end
+
+  it "should respond to release_bike method" do expect(dock).to respond_to(:release_bike) end
+
 
 end
