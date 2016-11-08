f2de608e185489cff6a5e79e74c51260662e1266
George Seeger <georgeseeger@hotmail.com>
2016-11-08 14:42:44 +0000

    boris bikes clean up

--- lib/boris_bikes.rb
+++ lib/boris_bikes.rb
@@ -1,7 +1,5 @@
 class DockingStation
 
-<<<<<<< HEAD
-=======
   def release_bike
   end
 
@@ -12,5 +10,4 @@ class Bike
   def working?
   end
 
->>>>>>> 1ccf2b02a7a8f3374a6a84e9de585062e2d6bc43
 end
