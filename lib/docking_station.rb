31d0874cfbd2646c1a1d8ec8b7642e6a9c7d1aac
Tom Fuller <tomf95@hotmail.co.uk>
2016-11-09 15:10:10 +0000

    added ability to dock bikes

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,9 +1,19 @@
 class DockingStation
-  
+
+  attr_reader :bikes
+
+  def initialize
+    @bikes = []
+  end
+
   def get_bike
     Bike.new
   end
 
   def release_bike
+  end
+
+  def dock_bike(bike)
+    @bikes << bike
   end
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -18,4 +18,16 @@ describe DockingStation do
     expect(subject.get_bike.working?).to eq true
   end
 
+  it 'should respond to dock_bike' do
+    expect(subject).to respond_to(:dock_bike).with(1).argument
+  end
+
+  it 'should contain a bikes array' do
+    expect(subject.bikes).to be_kind_of(Array)
+  end
+
+  it 'should add a docked bike to docked bikes array' do
+    bike = subject.get_bike
+    expect(subject.dock_bike(bike)).to include(bike)
+  end
 end
